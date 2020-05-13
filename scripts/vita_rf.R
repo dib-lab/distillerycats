ead9b06129a8208fcb57eb18cfab26b2b235d90e
taylorreiter <taylorreiter@gmail.com>
2020-05-13 12:50:21 -0700

    rm ibd refs

--- scripts/vita_rf.R
+++ scripts/vita_rf.R
@@ -16,57 +16,51 @@ set.seed(1)
 
 ## format hash table (samples x features)
 
-ibd <- read_feather(snakemake@input[['feather']]) # read in hash abund table
-ibd <- as.data.frame(ibd)                         # transform to dataframe
-ibd$sample <- gsub("_filt_named\\.sig", "", ibd$sample)
-rownames(ibd) <- ibd$sample                       # set sample as rownames
-ibd <- ibd[ , -ncol(ibd)]                         # remove the samples column
+hashes <- read_feather(snakemake@input[['feather']]) # read in hash abund table
+hashes <- as.data.frame(hashes)                      # transform to dataframe
+hashes$sample <- gsub("_filt_named\\.sig", "", hashes$sample)
+rownames(hashes) <- hashes$sample                    # set sample as rownames
+hashes <- hashes[ , -ncol(hashes)]                   # remove the samples column
 
 ## read in study metadata
-## collapse duplicate libraries so each sample only has one row
+## change "-" to "." in sample names, as some programs do this automatically
+## collapse duplicate samples so each sample only has one row
 info <- read_tsv(snakemake@input[['info']]) %>%
-  select(study_accession, library_name, diagnosis) %>%
-  mutate(library_name = gsub("\\-", "\\.", library_name)) %>%
-  filter(library_name %in% rownames(ibd)) %>%
+  mutate(sample = gsub("\\-", "\\.", sample)) %>%
+  filter(sample %in% rownames(hashes)) %>%
   distinct()
-
-## set validation cohort and remove it from variable selection
-# info_validation <- info %>%
-#   filter(study_accession == snakemake@params[["validation_study"]]) %>%
-#   mutate(library_name = gsub("-", "\\.", library_name))
-# ibd_validation <- ibd[rownames(ibd) %in% info_validation$library_name, ]
 
 ## remove validation cohort from variable selection
 info_novalidation <- info %>%
   filter(study_accession != snakemake@params[["validation_study"]]) %>%
-  mutate(library_name = gsub("-", "\\.", library_name))
-ibd_novalidation <- ibd[rownames(ibd) %in% info_novalidation$library_name, ]
+  mutate(sample = gsub("-", "\\.", sample))
+hashes_novalidation <- hashes[rownames(hashes) %in% info_novalidation$sample, ]
 
 ## make classification vector
-## match order of to ibd
-info_novalidation <- info_novalidation[match(rownames(ibd_novalidation), info_novalidation$library_name), ]
-## make diagnosis var
-diagnosis_novalidation <- info_novalidation$diagnosis
+## match order of to hashes
+info_novalidation <- info_novalidation[match(rownames(hashes_novalidation), info_novalidation$sample), ]
+## make var var
+var_novalidation <- info_novalidation$var
 
 # run vita ----------------------------------------------------------------
 
 ## perform variant selection
 ## var.sel.vita calculates p-values based on the empirical null distribution
 ## from non-positive VIMs as described in Janitza et al. (2015).
-ibd_vita <- var.sel.vita(x = ibd_novalidation, y = diagnosis_novalidation, p.t = 0.05,
-                        ntree = 10000, mtry.prop = 0.2, nodesize.prop = 0.1,
-                        no.threads = snakemake@params[["threads"]], 
-                        method = "ranger", type = "classification")
-saveRDS(ibd_vita, snakemake@output[["vita_rf"]])
+var_vita <- var.sel.vita(x = hashes_novalidation, y = var_novalidation, p.t = 0.05,
+                         ntree = 10000, mtry.prop = 0.2, nodesize.prop = 0.1,
+                         no.threads = snakemake@params[["threads"]], 
+                         method = "ranger", type = "classification")
+saveRDS(var_vita, snakemake@output[["vita_rf"]])
 
 # write files -------------------------------------------------------------
 
 ## write predictive hashes
-var <- ibd_vita$var                 # separate out selected predictive hashes
+var <- var_vita$var                 # separate out selected predictive hashes
 var <- gsub("X", "", var)           # remove the X from the beginning of hashes
 write.table(var, snakemake@output[['vita_vars']],
             quote = F, col.names = F, row.names = F)
 
 ## filter to predictive hashes and write to fie
-ibd_filt <- ibd[ , colnames(ibd) %in% var] # subset ibd to hashes in ibd_vita
-write.csv(ibd_filt, snakemake@output[['ibd_filt']], quote = F)
+hashes_filt <- hashes[ , colnames(hashes) %in% var] # subset ibd to hashes in ibd_vita
+write.csv(hashes_filt, snakemake@output[['var_filt']], quote = F)
